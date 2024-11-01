27f7486626f8fe28eb3bc9f9bdd77d99f663f116
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-01 16:44:10 -0700

    feat: add Tailwind CSS for styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = document.getElementById('root')
+
+if (root != null) {
+  createRoot(root).render(
     <App />
-  </StrictMode>,
-)
+  )
+}

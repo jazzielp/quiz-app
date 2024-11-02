cd6f19c29d849b5fd04f3ec01cbf1c148473ee7f
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-01 17:09:09 -0700

    feat: add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    color: {
+      purpol: '#A729F5',
+      darkNavy: '#313E51',
+      navy: '#3B4D66',
+      greyNavy: '#626C7F',
+      lightBluish: '#ABC1E1',
+      lightGrey: '#F4F6FA',
+      white: '#ffffff',
+      green: '##26D782',
+      red: '#EE5454'
+    },
     extend: {}
   },
   plugins: []

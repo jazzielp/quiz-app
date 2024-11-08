d735cdacf04e9fa3c781507d571efc817ca0c2b6
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-08 12:02:22 -0700

    feat: change types names

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,14 +1,14 @@
-export interface Welcome {
+export interface IQuizzes {
   quizzes: Quiz[]
 }
 
-export interface Quiz {
+export interface IQuiz {
   title: string
   icon: string
   questions: Question[]
 }
 
-export interface Question {
+export interface IQuestion {
   question: string
   options: string[]
   answer: string

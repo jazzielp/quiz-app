f0ad6a78a652eda9284276569f121ae36da5cd24
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-08 12:08:51 -0700

    feat: add hover to Quiz component

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 export function Quiz ({ quiz, img }: Props): JSX.Element {
   return (
-    <div className='group w-[528px] h-[92px] rounded-3xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px]'>
+    <div className='group w-[528px] h-[92px] rounded-3xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px] border-white hover:border-purpol transition-colors'>
       <img src={img} className='' />
       <p className='font-rubik font-medium text-[28px] flex-1'>{quiz}</p>
     </div>

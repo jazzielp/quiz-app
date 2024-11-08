068681191a7cea6706b587ac3c7bc31d03ea29eb
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-08 12:02:56 -0700

    feat: add Quiz component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import './App.css'
+import { useState } from 'react'
+import quizzesData from './db/data.json'
+import { IQuiz } from './types/types'
 import { Answer } from '@/components/Answer'
 import { Button } from '@/components/Buttons'
-
+import { Quiz } from '@/components/Quiz'
+console.log(quizzesData)
 function App (): JSX.Element {
+  const [quizzes] = useState<IQuiz[]>(quizzesData.quizzes)
+  console.log('🚀 ~ App ~ quizzes:', quizzes[0])
+
   return (
     <>
       <h1>Hello world</h1>
       <p className='font-rubik'>alsdkasdlasdkaslkdlasd</p>
       <Button text='Button Idle' />
       <Answer answer='cc' option='cc' number={1} selected />
+      <Quiz quiz='html' img={quizzes[0].icon} />
     </>
   )
 }
---
+++ src/components/Quiz.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  quiz: string
+  img?: string
+}
+export function Quiz ({ quiz, img }: Props): JSX.Element {
+  return (
+    <div className='group w-[528px] h-[92px] rounded-3xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px]'>
+      <img src={img} className='' />
+      <p className='font-rubik font-medium text-[28px] flex-1'>{quiz}</p>
+    </div>
+  )
+}

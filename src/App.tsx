c04c241bf79acbcafb7fc422cec8db24dd39572b
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-09 10:42:48 -0700

    feat: Implement header

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import './App.css'
 import { useState } from 'react'
 import quizzesData from './db/data.json'
 import { IQuiz } from './types/types'
-import { Answer } from '@/components/Answer'
-import { Button } from '@/components/Buttons'
-import { Quiz } from '@/components/Quiz'
+import { Header } from './components/Header'
 console.log(quizzesData)
 function App (): JSX.Element {
   const [quizzes] = useState<IQuiz[]>(quizzesData.quizzes)
@@ -12,11 +10,7 @@ function App (): JSX.Element {
 
   return (
     <>
-      <h1>Hello world</h1>
-      <p className='font-rubik'>alsdkasdlasdkaslkdlasd</p>
-      <Button text='Button Idle' />
-      <Answer answer='cc' option='cc' number={1} selected />
-      <Quiz quiz='html' img={quizzes[0].icon} />
+      <Header />
     </>
   )
 }

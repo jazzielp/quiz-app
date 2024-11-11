ed91657fbfb8d2410b1f437dc8a58aededb6d079
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-11 11:41:43 -0700

    fean: clean the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
-import { useState } from 'react'
-import quizzesData from './db/data.json'
-import { IQuiz } from './types/types'
-import { Header } from './components/Header'
-console.log(quizzesData)
-function App (): JSX.Element {
-  const [quizzes] = useState<IQuiz[]>(quizzesData.quizzes)
-  console.log('🚀 ~ App ~ quizzes:', quizzes[0])
+import { useAppSelector } from '@/hooks/store'
 
+import { Header } from '@/components/Header'
+
+function App (): JSX.Element {
+  const { quizzes } = useAppSelector((state) => state.quizzes)
+  console.log('🚀 ~ App ~ quizzes:', quizzes)
   return (
     <>
       <Header />

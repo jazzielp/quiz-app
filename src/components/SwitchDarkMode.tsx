7ad422484198d8c32162197f851949dab5eb0e25
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-11 12:00:20 -0700

    feat: add conditional for dark mode

--- src/components/SwitchDarkMode.tsx
+++ src/components/SwitchDarkMode.tsx
@@ -1,14 +1,19 @@
-import { IconMoodDark } from '@/assets/icons/IconMoonDark'
+import { IconMoonDark } from '@/assets/icons/IconMoonDark'
+import { IconMoonLight } from '@/assets/icons/IconMoonLight'
 import { IconSunDark } from '@/assets/icons/IconSunDark'
+import { IconSunLight } from '@/assets/icons/IconSunLight'
+import { MODE } from '@/consts/consts'
+import { useAppSelector } from '@/hooks/store'
 
 export function SwitchDarkMode (): JSX.Element {
+  const { mode } = useAppSelector((state) => state.quizzes)
   return (
     <div className='flex items-center justify-between gap-3'>
-      <IconSunDark />
+      {mode === MODE.light ? <IconSunDark /> : <IconSunLight />}
       <div className='w-8 rounded-[999px] p-1 bg-purpol'>
         <div className='size-3 bg-white rounded-full' />
       </div>
-      <IconMoodDark />
+      {mode === MODE.light ? <IconMoonDark /> : <IconMoonLight />}
     </div>
   )
 }

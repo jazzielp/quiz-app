5b4c479aaa92e76523e57341e90b5460315a353d
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-11 11:59:11 -0700

    feat: remove test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import './App.css'
-import { useAppSelector } from '@/hooks/store'
 
 import { Header } from '@/components/Header'
 
 function App (): JSX.Element {
-  const { quizzes } = useAppSelector((state) => state.quizzes)
-  console.log('🚀 ~ App ~ quizzes:', quizzes)
   return (
     <>
       <Header />

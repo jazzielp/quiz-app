d6e0edf8bf0f043e3f5ee361dc61ba24736b6372
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-15 11:13:35 -0700

    feat: implement darkmode service

--- src/components/SwitchDarkMode.tsx
+++ src/components/SwitchDarkMode.tsx
@@ -6,6 +6,7 @@ import { MODE } from '@/consts/consts'
 import { ModeType } from '@/types/types'
 import { useAppSelector } from '@/hooks/store'
 import { useQuizzesActions } from '@/hooks/useQuizzes'
+import { setDarkModeInHTML } from '@/services/darkMode'
 
 export function SwitchDarkMode (): JSX.Element {
   const { mode } = useAppSelector((state) => state.quizzes)
@@ -14,12 +15,13 @@ export function SwitchDarkMode (): JSX.Element {
   const handleClick = (mode: ModeType): void => {
     const sendMode = mode === MODE.light ? MODE.dark : MODE.light
     setDarkMode(sendMode)
+    setDarkModeInHTML(sendMode)
   }
   return (
     <div onClick={() => handleClick(mode)} className='flex items-center justify-between gap-3'>
       {mode === MODE.light ? <IconSunDark /> : <IconSunLight />}
-      <div className='w-8 rounded-[999px] p-1 bg-purpol'>
-        <div className='size-3 bg-white rounded-full' />
+      <div className='w-8 rounded-[999px] p-1 bg-purpol hover:cursor-pointer'>
+        <div className={`size-3 bg-white rounded-full transform transition-transform duration-300 ease-in-out ${mode === MODE.dark ? 'translate-x-3' : 'translate-x-0'}`} />
       </div>
       {mode === MODE.light ? <IconMoonDark /> : <IconMoonLight />}
     </div>

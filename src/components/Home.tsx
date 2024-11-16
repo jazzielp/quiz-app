a47d83414da4ef7a357d124c92db59558b6f2f87
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-16 12:09:53 -0700

    feat: add responsive design for tablet devices

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,12 +8,18 @@ export function Home (): JSX.Element {
     <>
       <Header />
       <Main>
-        {
+        <div className=''>
+          <h1 className='w-72 md:w-[444px] lg:w-[465px] text-[40px] md:text-[64px] text-darkNavy dark:text-white text-wrap font-light'>Welcome to the <span className='font-medium'>Frontend Quiz!</span></h1>
+          <p className='text-lg md:text-xl text-greyNavy italic dark:text-lightBluish mt-4 lg:mt-12'>Pick a subject to get started.</p>
+        </div>
+        <div className='mt-10 lg:m-0 md:mt-16 flex gap-3 md:gap-6 flex-col'>
+          {
           quizzes?.map((quiz) => {
             const { title, icon, color } = quiz
             return <SelectQuiz key={quiz.title} title={title} img={icon} color={color} />
           })
         }
+        </div>
       </Main>
     </>
   )
--- src/components/SelectQuiz.tsx
+++ src/components/SelectQuiz.tsx
@@ -5,11 +5,11 @@ interface Props {
 }
 export function SelectQuiz ({ title, img, color }: Props): JSX.Element {
   return (
-    <div className='group bg-white w-[327px]  h-[64px] rounded-xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px] border-white hover:border-purpol transition-colors'>
-      <picture className={`size-10 rounded-lg grid place-items-center bg-[${color}]`}>
-        <img src={img} className='size-8' />
+    <div className=' bg-white dark:bg-navy w-[327px] h-[64px] md:w-[640px] md:h-[80px] lg:w-[564px] lg:h[96px] rounded-xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px] border-white dark:border-navy hover:border-purpol dark:hover:border-purpol transition-colors'>
+      <picture className={`size-10 md:size-14 rounded-lg grid place-items-center bg-[${color}]`}>
+        <img src={img} className='size-8 md:size-10' />
       </picture>
-      <p className='font-rubik font-medium text-lg flex-1 dark:text-white'>{title}</p>
+      <p className='font-rubik font-medium text-lg md:text-[28px] flex-1 dark:text-white'>{title}</p>
     </div>
   )
 }

af1b605bf7d2a24d9208a2c36b8ba8c356827e95
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-16 10:10:00 -0700

    feat: Implement Home component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { SwitchDarkMode } from './SwitchDarkMode'
 import { TitleQuize } from './TitleQuize'
 export function Header (): JSX.Element {
   return (
-    <header className='flex items-center justify-between w-[375px] h-[72px] m-auto'>
+    <header className='flex items-center justify-between max-w-[1157px] min-w-[375px] px-6 md:px-16 lg: h-[72px] m-auto'>
       <TitleQuize />
       <SwitchDarkMode />
     </header>

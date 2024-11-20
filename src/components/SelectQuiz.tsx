d63503adbf88977b45bbb5af4b93f581f036da33
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-19 17:09:52 -0700

    feat: Any change

--- src/components/SelectQuiz.tsx
+++ src/components/SelectQuiz.tsx
@@ -1,12 +1,15 @@
+import '@/components/SelectQuiz.css'
+import { BG_COLOR_CURSOS } from '@/consts/consts'
 interface Props {
-  title: string
+  title: keyof typeof BG_COLOR_CURSOS
   img: string
-  color: string
 }
-export function SelectQuiz ({ title, img, color }: Props): JSX.Element {
+
+export function SelectQuiz ({ title, img }: Props): JSX.Element {
+  const bgColor = BG_COLOR_CURSOS[title]
   return (
     <div className=' bg-white dark:bg-navy w-[327px] h-[64px] md:w-[640px] md:h-[80px] lg:w-[564px] lg:h[96px] rounded-xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px] border-white dark:border-navy hover:border-purpol dark:hover:border-purpol transition-colors'>
-      <picture className={`size-10 md:size-14 rounded-lg grid place-items-center bg-[${color}]`}>
+      <picture className={`size-10 md:size-14 rounded-lg grid place-items-center ${bgColor}`}>
         <img src={img} className='size-8 md:size-10' />
       </picture>
       <p className='font-rubik font-medium text-lg md:text-[28px] flex-1 dark:text-white'>{title}</p>
--- src/consts/consts.tsx
+++ src/consts/consts.tsx
@@ -4,3 +4,10 @@ export const MODE = {
   dark: 'dark',
   light: 'light'
 } as const
+
+export const BG_COLOR_CURSOS = {
+  HTML: 'bgHtml',
+  CSS: 'bgCss',
+  JavaScript: 'bgJs',
+  Accessibility: 'bgAccessibility'
+} as const

0d71ea079c144924887a98cf222f8c8868e4cfe8
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-25 11:06:22 -0700

    style: update element styling for improved appearance

--- src/components/TitleQuize.tsx
+++ src/components/TitleQuize.tsx
@@ -8,11 +8,11 @@ interface Props {
 export function TitleQuize ({ quiz, img }: Props): JSX.Element {
   const bgColor = BG_COLOR_QUIZZES[quiz as keyof typeof BG_COLOR_QUIZZES]
   return (
-    <div className='flex items-center gap-6 dark:text-white'>
+    <div className='flex items-center gap-4 md:gap-6 dark:text-white'>
       <picture className={`size-10 md:size-14 rounded-lg grid place-items-center ${bgColor}`}>
-        <img src={img} className='size-8 md:size-10' />
+        <img src={img} className='size-7 md:size-10' />
       </picture>
-      <p className='font-rubik font-medium text-[28px] flex-1'>{quiz}</p>
+      <p className='font-rubik text-darkNavy dark:text-white font-medium text-lg md:text-[28px] flex-1'>{quiz}</p>
     </div>
   )
 }

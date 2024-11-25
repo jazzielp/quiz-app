a6d17b315648d09b1dc63aa0a816df77213ef91f
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-25 11:03:49 -0700

    feat: add responsive margin-top adjustment for element

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export function Header (): JSX.Element {
   }
 
   return (
-    <header className='flex items-center justify-between xl:w-[1157px] md:w-[640px] w-[375px] px-6 md:px-0 h-[72px] m-auto mt-9'>
+    <header className='flex items-center justify-between xl:w-[1157px] md:w-[640px] w-[375px] px-6 md:px-0 h-[72px] mx-auto md:mt-4 xl:mt-9'>
       {quiz !== null ? <TitleQuize quiz={title} img={image} /> : <div />}
       <SwitchDarkMode />
     </header>

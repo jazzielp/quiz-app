56e5e62e3d7c4e82bb20f0199f27f13a90373beb
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-30 11:54:33 -0700

    feat: funtionality to select an answer

--- src/components/SelectAnswer.tsx
+++ src/components/SelectAnswer.tsx
@@ -3,6 +3,7 @@ import { Correct } from '@/assets/icons/Correct'
 import { useEffect, useState } from 'react'
 import { Error } from '@/assets/icons/Error'
 import { useAppSelector } from '@/hooks/store'
+import { useQuizzesActions } from '@/hooks/useQuizzes'
 interface Params {
   correctAnswer: string
   option: string
@@ -12,6 +13,8 @@ interface Params {
 
 export function SelectAnswer ({ correctAnswer, option, number, selected }: Params): JSX.Element {
   const { userAnswer } = useAppSelector((state) => state.quizzes)
+  const { setUserAnswer } = useQuizzesActions()
+
   const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null)
   const [spanStyle, setSpanStyle] = useState('')
   const [divStyle, setDivStyle] = useState('')
@@ -19,9 +22,14 @@ export function SelectAnswer ({ correctAnswer, option, number, selected }: Param
   const letter = LETTERS[number]
 
   useEffect(() => {
-    if (userAnswer === '') {
-      setDivStyle('border-white')
+    if (userAnswer !== option) {
+      console.log('no seleccionada')
+      setDivStyle('dark:border-navy border-white')
       setSpanStyle('bg-lightGrey text-greyNavy')
+    } else if (userAnswer !== '' && userAnswer === option) {
+      setSpanStyle('bg-purpol text-white')
+      setDivStyle('border-purpol')
+      console.log('Seleccionada')
     }
     // else if (isAnswerCorrect !== null && isAnswerCorrect) {
     //   spanStyle = 'bg-green text-white'
@@ -33,10 +41,14 @@ export function SelectAnswer ({ correctAnswer, option, number, selected }: Param
     //   spanStyle = 'bg-purpol text-white'
     //   divStyle = 'border-purpol'
     // }
-  }, [])
+  }, [userAnswer, option])
+
+  const handleClickOption = (answer: string): void => {
+    setUserAnswer(answer)
+  }
 
   return (
-    <div className={`group w-[528px] bg-white rounded-3xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px] ${divStyle}`}>
+    <div onClick={() => handleClickOption(option)} className={`group w-[528px] bg-white rounded-3xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px] dark:bg-navy ${divStyle}`}>
       <span className={`size-14 flex justify-center items-center rounded-lg ${selected ? '' : 'group-hover:bg-[#F6E7FF] transition-colors group-hover:text-purpol'} font-medium font-rubik text-[28px] ${spanStyle}`}>
         {letter}
       </span>

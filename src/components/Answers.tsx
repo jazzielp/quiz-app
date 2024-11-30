ad778b9d500c7b31fd95fb6c1590513cface7fb7
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-11-30 11:53:33 -0700

    feat: validate select answer

--- src/components/Answers.tsx
+++ src/components/Answers.tsx
@@ -2,11 +2,13 @@ import { useAppSelector } from '@/hooks/store'
 import { SelectAnswer } from './SelectAnswer'
 import { useEffect, useState } from 'react'
 import { Error } from '@/assets/icons/Error'
+import { useQuizzesActions } from '@/hooks/useQuizzes'
 
 export function Answers (): JSX.Element {
   const { quiz, indexQuestion } = useAppSelector((state) => state.quizzes)
   const [options, setOptions] = useState<string[]>()
   const [correctAnswer, setCorrectAnswer] = useState<string>('')
+  const [isEmptyAnswer, setIsEmptyAnswer] = useState(false)
   useEffect(() => {
     if (quiz !== null) {
       const { questions } = quiz
@@ -14,6 +16,7 @@ export function Answers (): JSX.Element {
       setCorrectAnswer(questions[indexQuestion].answer)
     }
   }, [quiz])
+
   return (
     <div className='flex flex-col gap-6'>
 
@@ -23,7 +26,11 @@ export function Answers (): JSX.Element {
           ))
         }
       <button className='w-full h-[92px] rounded-3xl bg-purpol hover:opacity-50 transition-opacity text-[28px] text-white font-rubik font-medium'>Submit Answer</button>
-      <div className='flex items-center justify-center gap-2'> <Error /> <p className='text-red font-rubik text-2xl'>Please select an answer</p>  </div>
+      {
+        isEmptyAnswer
+          ? <div className='flex items-center justify-center gap-2'> <Error /> <p className='text-red font-rubik text-2xl'>Please select an answer</p>  </div>
+          : null
+      }
 
     </div>
   )

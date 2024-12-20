337f8eeeb10f90620cdfd3a6e78e935adc775cea
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-12-20 16:48:33 -0700

    feat: Refactor SelectAnswer component to improve answer validation logic and styling

--- src/components/SelectAnswer.tsx
+++ src/components/SelectAnswer.tsx
@@ -8,11 +8,10 @@ interface Params {
   correctAnswer: string
   option: string
   number: number
-  selected: boolean
   isAnswerCorrect: boolean | undefined
 }
 
-export function SelectAnswer ({ correctAnswer, option, number, selected, isAnswerCorrect }: Params): JSX.Element {
+export function SelectAnswer ({ correctAnswer, option, number, isAnswerCorrect }: Params): JSX.Element {
   const { userAnswer } = useAppSelector((state) => state.quizzes)
   const { setUserAnswer } = useQuizzesActions()
   const [spanStyle, setSpanStyle] = useState('')
@@ -24,23 +23,25 @@ export function SelectAnswer ({ correctAnswer, option, number, selected, isAnswe
     if (userAnswer !== option) {
       setDivStyle('dark:border-navy border-white')
       setSpanStyle('bg-lightGrey text-greyNavy')
-    } else if (userAnswer !== '' && userAnswer === option) {
+    } else if (userAnswer !== '' && userAnswer === option && isAnswerCorrect === undefined) {
+      console.log('entra en ', option)
       setSpanStyle('bg-purpol text-white')
       setDivStyle('border-purpol')
     } else if (isAnswerCorrect === true && option === correctAnswer) {
       setSpanStyle('bg-green text-white')
       setDivStyle('border-green')
+    } else if (isAnswerCorrect === false && userAnswer === option) {
+      setSpanStyle('bg-red text-white')
+      setDivStyle('border-red')
     }
-    // else if (isAnswerCorrect !== null && !isAnswerCorrect) {
-    //   spanStyle = 'bg-red text-white'
-    //   divStyle = 'border-red'
-    // } else if (selected) {
+    // else if (selected) {
     //   spanStyle = 'bg-purpol text-white'
     //   divStyle = 'border-purpol'
     // }
-  }, [userAnswer, option])
+  }, [userAnswer, option, isAnswerCorrect])
 
   const handleClickOption = (answer: string): void => {
+    if (isAnswerCorrect !== undefined) return
     setUserAnswer(answer)
   }
 
@@ -49,18 +50,15 @@ export function SelectAnswer ({ correctAnswer, option, number, selected, isAnswe
   }, [isAnswerCorrect])
 
   return (
-    <div onClick={() => handleClickOption(option)} className={`group w-[528px] bg-white rounded-3xl p-[18px] flex justify-between hover:cursor-pointer items-center gap-8 border-[3px] dark:bg-navy ${divStyle}`}>
-      <span className={`size-14 flex justify-center items-center rounded-lg ${selected ? '' : 'group-hover:bg-[#F6E7FF] transition-colors group-hover:text-purpol'} font-medium font-rubik text-[28px] ${spanStyle}`}>
+    <div onClick={() => handleClickOption(option)} className={`group w-[528px] bg-white rounded-3xl p-[18px] flex justify-between ${isAnswerCorrect === undefined ? 'hover:cursor-pointer' : ''} items-center gap-8 border-[3px] dark:bg-navy ${divStyle}`}>
+      <span className={`size-14 flex justify-center items-center rounded-lg ${isAnswerCorrect === undefined ? 'group-hover:bg-[#F6E7FF] transition-colors group-hover:text-purpol' : ''} font-medium font-rubik text-[28px] ${spanStyle}`}>
         {letter}
       </span>
       <p className='font-rubik leading-8 dark:text-white font-medium text-[28px] flex-1'>{option}</p>
       {isAnswerCorrect === null && null}
       {isAnswerCorrect === true && correctAnswer === option && <Correct />}
-      {
-        isAnswerCorrect === undefined
-          ? null
-          : isAnswerCorrect ? <Correct /> : <Error />
-      }
+      {isAnswerCorrect === false && correctAnswer === option && <Correct />}
+      {isAnswerCorrect === false && userAnswer === option && <Error />}
     </div>
   )
 }

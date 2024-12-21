ee996f1b2e62af02178be46b606122488418c3c8
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-12-21 09:04:15 -0700

    feat: Add QuizCompleted component and enhance quiz state management

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import './App.css'
 import { Home } from '@/components/Home'
 import { useAppSelector } from './hooks/store'
 import { Quiz } from './components/Quiz'
+import { QuizCompleted } from './components/QuizCompleted'
+import { useEffect, useState } from 'react'
 
 function App (): JSX.Element {
-  const { quiz } = useAppSelector((state) => state.quizzes)
+  const { quiz, indexQuestion } = useAppSelector((state) => state.quizzes)
+  const [totalQuestion, setTotalQuestion] = useState(0)
+
+  useEffect(() => {
+    if (quiz?.questions.length !== undefined) {
+      setTotalQuestion(quiz?.questions.length)
+    }
+  }, [quiz, indexQuestion])
   return (
     <>
+      {totalQuestion >= indexQuestion && <QuizCompleted />}
       {
         quiz !== null ? <Quiz /> : <Home />
       }

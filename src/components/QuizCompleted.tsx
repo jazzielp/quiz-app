7280e4215e86b179897aa4c2ba9f33e9666e8c5d
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2025-01-23 16:31:02 -0700

    feat: Enhance QuizCompleted component with Header, Main, and QuizResult integration

--- src/components/QuizCompleted.tsx
+++ src/components/QuizCompleted.tsx
@@ -1,5 +1,20 @@
+import { Header } from './Header'
+import { Main } from './Main'
+import { QuizResult } from './QuizResult'
+
 export function QuizCompleted (): JSX.Element {
   return (
-    <h1>Quiz Completed</h1>
+    <>
+      <Header />
+      <Main>
+        <div className=''>
+          <h1 className='w-72 md:w-[444px] lx:w-[465px] text-[40px] leading-tight  md:text-[64px] text-darkNavy dark:text-white text-wrap font-light'>Quiz completed <span className='font-medium'>You scored...</span></h1>
+        </div>
+        <div className='xl:w-[528px] md:w-[640px] w-[327px]'>
+          <QuizResult />
+          <button className='w-full h-[92px] rounded-3xl bg-purpol hover:opacity-50 transition-opacity text-[28px] text-white font-rubik font-medium'>Play again</button>
+        </div>
+      </Main>
+    </>
   )
 }

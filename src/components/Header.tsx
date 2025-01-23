fdd1261492843ff3518b3f947e6336f0ed72754c
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2025-01-23 16:30:26 -0700

    refactor: Simplify Header component by removing unused title and image variables

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,10 @@ import { TitleQuize } from './TitleQuize'
 
 export function Header (): JSX.Element {
   const { quiz } = useAppSelector((state) => state.quizzes)
-  let title = ''
-  let image = ''
-  if (quiz !== null) {
-    title = quiz.title
-    image = quiz.icon
-  }
 
   return (
     <header className='flex items-center justify-between xl:w-[1157px] md:w-[640px] w-[375px] px-6 md:px-0 h-[72px] mx-auto md:mt-4 xl:mt-9'>
-      {quiz !== null ? <TitleQuize quiz={title} img={image} /> : <div />}
+      {quiz !== null ? <TitleQuize /> : <div />}
       <SwitchDarkMode />
     </header>
   )

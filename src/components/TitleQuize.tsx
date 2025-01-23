19d161e61a7ad04137e04b52a8285a77994839aa
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2025-01-23 16:31:42 -0700

    refactor: Update TitleQuize component to use Redux state for quiz title and icon

--- src/components/TitleQuize.tsx
+++ src/components/TitleQuize.tsx
@@ -1,18 +1,16 @@
 import { BG_COLOR_QUIZZES } from '@/consts/consts'
+import { useAppSelector } from '@/hooks/store'
 
-interface Props {
-  quiz?: string
-  img?: string
-}
-
-export function TitleQuize ({ quiz, img }: Props): JSX.Element {
-  const bgColor = BG_COLOR_QUIZZES[quiz as keyof typeof BG_COLOR_QUIZZES]
+export function TitleQuize (): JSX.Element {
+  const { quiz } = useAppSelector((state) => state.quizzes)
+  const { title, icon } = (quiz != null) ? quiz : { title: '', icon: '' }
+  const bgColor = BG_COLOR_QUIZZES[title as keyof typeof BG_COLOR_QUIZZES]
   return (
     <div className='flex items-center gap-4 md:gap-6 dark:text-white'>
       <picture className={`size-10 md:size-14 rounded-lg grid place-items-center ${bgColor}`}>
-        <img src={img} className='size-7 md:size-10' />
+        <img src={icon} className='size-7 md:size-10' />
       </picture>
-      <p className='font-rubik text-darkNavy dark:text-white font-medium text-lg md:text-[28px] flex-1'>{quiz}</p>
+      <p className='font-rubik text-darkNavy dark:text-white font-medium text-lg md:text-[28px] flex-1'>{title}</p>
     </div>
   )
 }
